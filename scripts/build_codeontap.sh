9ff9e23fa554cda5ee59b88af229b713f6d95e03
roleyfoley <roleyfoley@gmail.com>
2018-05-01 20:57:28 +1000

    File permissiosn fix

--- scripts/build_codeontap.sh
+++ scripts/build_codeontap.sh
@@ -1,5 +1,7 @@
  # Create the Git clone commands to get the Repositories
 jq -r '.Repositories[] | "git clone --depth 1 --branch \(.Branch) \(.Repository) \(.Directory)" ' </build/config.json > /build/scripts/clone.sh
 
+chmod u+rwx /build/scripts/clone.sh 
+
 # Create the Version file from the config
 jq '{ "FrameworkVersion" : .FrameworkVersion }' </build/config.json > /var/opt/codeontap/version.json

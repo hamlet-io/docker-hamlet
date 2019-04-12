14663750d875299d93e76cf361a198c961d93d77
roleyfoley <roleyfoley@gmail.com>
2019-04-12 16:21:32 +1000

    move framework clone to tag based over config

--- scripts/base/build_codeontap.sh
+++ scripts/base/build_codeontap.sh
@@ -1,7 +1,14 @@
- # Create the Git clone commands to get the Repositories
-jq -r '.Repositories[] | "git clone --depth 1 --branch \(.Branch) \(.Repository) \(.Directory)" ' </build/config.json > /build/scripts/clone.sh
+#!/usr/bin/env bash
 
+# Override docker latest for git repo latest
+CODEONTAP_VERSION="${CODEONTAP_VERSION}"
+if [[ "${CODEONTAP_VERSION}" == "latest" ]]; then 
+    CODEONTAP_VERSION="master"
+fi
+
+# Create the Git clone commands to get the Repositories
+jq  -r '.Repositories[] | "git clone --depth 1 --branch \(env.CODEONTAP_VERSION) \(.Repository) \(.Directory)" ' </build/config.json > /build/scripts/clone.sh
 chmod u+rwx /build/scripts/clone.sh 
 
 # Create the Version file from the config
-jq '{ "FrameworkVersion" : .FrameworkVersion }' </build/config.json > /opt/codeontap/version.json
+echo "{}" | jq  '{ "FrameworkVersion" : env.CODEONTAP_VERSION, "ContainerVersion" : env.DOCKER_IMAGE_VERSION  }' > /opt/codeontap/version.json

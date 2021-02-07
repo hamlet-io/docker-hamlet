fc51db2092bcaacbf0b73dd5131ee5d3e102ecc4
Ross Murray <ross.murray@protonmail.com>
2021-02-08 10:19:39 +1100

    feat: Include Diagrams Plugin (#32)
    * feat: incl diagrams plugin by default
    
    The Hamlet Diagrams plugin is being incorporated into the CLI in
    hamlet-io/executor-python#75
    
    This adds the diagrams plugin repository into the list of cloned-
    repositories that are injected into the docker container on build,
    ensuring that it will be available for the CLI to use.
    
    * refactor: incl container env path to diagrams plugin
    
    * refactor: allow for default repo branch as master or main
    
    Co-authored-by: Ross Murray <rossmurray@protonmail.com>

--- scripts/base/build_hamlet.sh
+++ scripts/base/build_hamlet.sh
@@ -7,7 +7,11 @@ if [[ "${HAMLET_VERSION}" == "latest" ]]; then
 fi
 
 # Create the Git clone commands to get the Repositories
-jq  -r '.Repositories[] | "git clone --depth 1 --branch \(env.HAMLET_VERSION) \(.Repository) \(.Directory)" ' </build/config.json > /build/scripts/clone.sh
+# note: original hamlet-io repositories use "master" as default branch
+#       whilst the modern approach is to use "main" as default.
+#       This checks for the existence of the HAMLET_VERSION branch and
+#       if it does not exist, it attempts "main" instead.
+jq -r '.Repositories[] | "git ls-remote --heads \(.Repository) \(env.HAMLET_VERSION) && git clone --depth 1 --branch \(env.HAMLET_VERSION) \(.Repository) \(.Directory) || git ls-remote --heads \(.Repository) main && git clone --depth 1 --branch main \(.Repository) \(.Directory)" ' </build/config.json > /build/scripts/clone.sh
 chmod u+rwx /build/scripts/clone.sh
 
 # Create the Version file from the config

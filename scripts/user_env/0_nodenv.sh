91000308bb674e6533e4ee79af01475f8d6ccd7b
roleyfoley <roleyfoley@gmail.com>
2021-06-11 23:21:17 +1000

    fix: include plugins for global node packages
    Add nodenv plugins which handle the shim processing for global
    node packges. This adds a hook in npm which tells nodenv to update
    the available binaries on global installs
    
    Adds yarn and npm ( update ) as default packages for all node versions
    installed. This will be automatically done as part of the install process

--- scripts/user_env/0_nodenv.sh
+++ scripts/user_env/0_nodenv.sh
@@ -3,11 +3,18 @@ set -eo pipefail
 
 NODENV_ROOT="${NODENV_ROOT:-"${HOME}/.nodenv"}"
 
+# Base nodenv and a couple of plugins to handle global packages and manage installations
 git clone --depth 1 https://github.com/nodenv/nodenv.git "${NODENV_ROOT}"
 git clone --depth 1 https://github.com/nodenv/node-build.git "${NODENV_ROOT}/plugins/node-build"
 git clone --depth 1 https://github.com/nodenv/node-build-update-defs.git "${NODENV_ROOT}/plugins/node-build-update-defs"
+git clone --depth 1 https://github.com/nodenv/nodenv-package-rehash.git "${NODENV_ROOT}/plugins/nodenv-package-rehash"
+git clone --depth 1 https://github.com/nodenv/nodenv-default-packages.git "${NODENV_ROOT}/plugins/nodenv-default-packages"
 
 ( cd ${NODENV_ROOT} && src/configure && make -C src )
+
+# install package managers in all nodenv versions
+echo "npm" >> "${NODENV_ROOT}/default-packages"
+echo "yarn" >> "${NODENV_ROOT}/default-packages"
 
 # nodenv install
 NODE_VERSION=12.22.1
@@ -15,3 +22,5 @@ eval "$(nodenv init -)"
 nodenv install "${NODE_VERSION}"
 nodenv global "${NODE_VERSION}"
 node --version
+
+nodenv package-hooks install --all

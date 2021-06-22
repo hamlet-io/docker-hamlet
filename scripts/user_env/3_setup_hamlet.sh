30147a3e16f56f888d836d95abc63f2135bf7f5a
roleyfoley <roleyfoley@gmail.com>
2021-06-22 15:03:43 +1000

    fix: set default engine to tram

--- scripts/user_env/3_setup_hamlet.sh
+++ scripts/user_env/3_setup_hamlet.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-HAMLET_ENGINE="${HAMLET_ENGINE:-"unicycle"}"
+HAMLET_ENGINE="${HAMLET_ENGINE:-"tram"}"
 
 # make sure the config dir exists for volume mounts
 mkdir -p "${HOME}/.hamlet/config"

4a59130def9158c61393aa37a254ed22db21a79c
roleyfoley <roleyfoley@gmail.com>
2021-07-07 11:12:44 +1000

    refactor: hamlet cli and engine management
    - moves to using standard releases of the hamlet cli
    - use the default engine version defined by the cli instead of overriding it
    - add test to make sure that the engine can be called and generate outputs
    - enable automatic updates of the engine to reduce the need to rebuild the container
    - Use the standard install process to setup the engine

--- scripts/user_env/3_setup_hamlet.sh
+++ scripts/user_env/3_setup_hamlet.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-HAMLET_ENGINE="${HAMLET_ENGINE:-"tram"}"
+pip install "hamlet-cli>=9.1.1,<10.0.0"
 
-# make sure the config dir exists for volume mounts
-mkdir -p "${HOME}/.hamlet/config"
+# Setup a base engine to get things going
+hamlet engine install-engine
 
-# Install and set a default hamlet engine so that things work out of the box
-pip install --pre hamlet-cli
-hamlet engine install-engine "${HAMLET_ENGINE}"
-hamlet engine set-engine "${HAMLET_ENGINE}"
-
-# Basic smoke tests to ensure global engine is working
-[[ "$(hamlet engine env GENERATION_DIR)" == "${GENERATION_DIR}" ]] || ( echo "GENERATION_DIR environment variable doesn't match cli expected variable"; exit 255 )
+# Snoke tests to ensure we have a base setup in place
+[[ "$(hamlet engine env GENERATION_DIR)" == "${GENERATION_DIR}" ]] || ( echo "GENERATION_DIR environment variable doesn't match cli expected variable"; hamlet engine env; echo "Current Env: ${GENERATION_DIR}"; exit 255 )
 [[ -d "$(hamlet engine env GENERATION_DIR)" ]] || ( echo "No global engine dir found"; exit 255 )
-[[ "$(ls -A $(hamlet engine env GENERATION_DIR)))" ]] || ( echo "Global engine dir empty"; exit 255 )
+ls -A $(hamlet engine env GENERATION_DIR) || ( echo "Global engine dir empty"; exit 255 )
+hamlet entrance list-entrances || ( echo "Could not list entrances with call through to engine"; exit 255 )

642fa79696e81530a46ed6438a194244e4adf54e
roleyfoley <roleyfoley@gmail.com>
2021-07-09 19:37:11 +1000

    fix: testing for handling explicit set engine vs global

--- scripts/user_env/3_setup_hamlet.sh
+++ scripts/user_env/3_setup_hamlet.sh
@@ -7,7 +7,11 @@ pip install "hamlet-cli>=9.1.1,<10.0.0"
 hamlet engine install-engine
 
 # Snoke tests to ensure we have a base setup in place
-[[ "$(hamlet engine env GENERATION_DIR)" == "${GENERATION_DIR}" ]] || ( echo "GENERATION_DIR environment variable doesn't match cli expected variable"; hamlet engine env; echo "Current Env: ${GENERATION_DIR}"; exit 255 )
-[[ -d "$(hamlet engine env GENERATION_DIR)" ]] || ( echo "No global engine dir found"; exit 255 )
-ls -A $(hamlet engine env GENERATION_DIR) || ( echo "Global engine dir empty"; exit 255 )
+[[ "$(hamlet --engine _global engine env GENERATION_DIR)" == "${GENERATION_DIR}" ]] || ( echo "GENERATION_DIR environment variable doesn't match cli expected variable"; hamlet engine env; echo "Current Env: ${GENERATION_DIR}"; exit 255 )
+[[ -d "$(hamlet --engine _global engine env GENERATION_DIR)" ]] || ( echo "No global engine dir found"; exit 255 )
+ls -A $(hamlet --engine _global engine env GENERATION_DIR) || ( echo "Global engine dir empty"; exit 255 )
+
+[[ -d "$(hamlet engine env GENERATION_DIR)" ]] || ( echo "No default engine dir found"; exit 255 )
+ls -A $(hamlet engine env GENERATION_DIR) || ( echo "default engine dir empty"; exit 255 )
+
 hamlet entrance list-entrances || ( echo "Could not list entrances with call through to engine"; exit 255 )

9306565231c193d62e9f94f0dff689b7fcf6f0d8
Michael Leditschke <mike@ammd.com.au>
2021-11-04 15:15:50 +1100

    fix: ignore meteor certificate issues
    Becuase we are building an old version of meteor, it is necessary to
    turn off some certificate checking during container build.

--- scripts/meteor/setup_meteor.sh
+++ scripts/meteor/setup_meteor.sh
@@ -3,6 +3,10 @@ set -euo pipefail
 
 METEOR_RELEASE="${METEOR_RELEASE:-"1.8.0.1"}"
 
+# Workaround for expired certificate with 1.8
+# https://docs.meteor.com/expired-certificate.html
+export NODE_TLS_REJECT_UNAUTHORIZED=0
+
 #Install Meteor
 curl https://install.meteor.com/?release=${METEOR_RELEASE} | sh
 

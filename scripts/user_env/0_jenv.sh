78a5b2046baaf0d64bb9ed916bc5b40bfab6f2ec
Michael Foley <roleyfoley@gmail.com>
2024-03-26 10:03:36 +1100

    fix: minor updates to docker image (#80)

--- scripts/user_env/0_jenv.sh
+++ scripts/user_env/0_jenv.sh
@@ -7,8 +7,9 @@ git clone --depth 1 https://github.com/jenv/jenv.git "${JENV_ROOT}"
 
 # pyenv install
 eval "$(jenv init -)"
+
 jenv add /usr/lib/jvm/temurin-11-jdk-amd64/
 jenv add /usr/lib/jvm/temurin-17-jdk-amd64/
 
+jenv doctor
 jenv global system
-jenv enable-plugin maven
